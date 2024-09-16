eca50dba9b0122e9dec1ccf5f5d8e3638d748f80
amsesk <amsesk@umich.edu>
2024-09-16 17:27:09 +0000

    add some more functions

--- R/grange_getters.R
+++ R/grange_getters.R
@@ -6,6 +6,13 @@ load_fragments <- function(ds,
   return(fragments)
 }
 
+#' @importFrom GenomicRanges strand
+#' @export
+get_gene_region_strand <- function(regions,
+                                   gene_name) {
+  as.vector(strand(regions[regions$gene_name == gene_name]))
+}
+
 #' @export
 get_gene_region_gr <- function(regionSet,
                                gene_name) {
@@ -39,18 +46,18 @@ get_gene_region_coverage <- function(fragments, regionSet, gene_name) {
 
 #' @importFrom GenomicRanges start end
 #' @export
-get_gene_tss <- function(gene_name, genome = "ChrAccRAnnotationHg38") {
-  if (genome != "ChrAccRAnnotationHg38") {
-    message("Unsupported genome specification: ", genome)
-    stop()
-  }
-  if (!require(genome, quietly = TRUE)) {
-    install.packages("https://muellerf.s3.amazonaws.com/data/ChrAccR/data/annotation/ChrAccRAnnotationHg38_0.0.1.tar.gz")
-  }
-  library(ChrAccRAnnotationHg38)
-  tssgr <- get("getGeneAnnotation", asNamespace(genome))(anno = "gencode_coding", type = "tssGr")
+get_gene_tss <- function(gene_name, tssgr) {
+  # if (genome != "ChrAccRAnnotationHg38") {
+  #   message("Unsupported genome specification: ", genome)
+  #   stop()
+  # }
+  # if (!require(genome, quietly = TRUE)) {
+  #   install.packages("https://muellerf.s3.amazonaws.com/data/ChrAccR/data/annotation/ChrAccRAnnotationHg38_0.0.1.tar.gz")
+  # }
+  # library(ChrAccRAnnotationHg38)
+  # tssgr <- get("getGeneAnnotation", asNamespace(genome))(anno = "gencode_coding", type = "tssGr")
   gene_tssgr <- tssgr[tssgr$gene_name == gene_name]
-  if (length(gene_tss) != 1 || start(gene_tss) != end(gene_tss)) {
+  if (length(gene_tssgr) != 1 || start(gene_tssgr) != end(gene_tssgr)) {
     message("Something wrong with TSS.")
     stop()
   }
@@ -63,7 +70,7 @@ get_gene_tss <- function(gene_name, genome = "ChrAccRAnnotationHg38") {
 #' @importFrom dplyr mutate select group_by summarize
 #' @importFrom tidyr unnest
 #' @export
-pool_gene_region_coverage = function(ds,
+pool_gene_region_coverage.DsATAC = function(ds,
                                      gene_name,
                                      groupby,
                                      regions) {
@@ -72,6 +79,32 @@ pool_gene_region_coverage = function(ds,
     mutate(region_coverage = lapply(sampleid, function(s, r, g, ds) {
                       message("Loading fragments for ", s)
                       frags = load_fragments(ds, s)
+                      cov = get_gene_region_coverage(frags, r, g)
+                      return(cov)
+                      }, regions, gene_name, ds))
+  metadata = metadata %>%
+    select(sampleid, {{ groupby }}, region_coverage) %>%
+    unnest(cols  = c(region_coverage))
+  metadata = metadata %>%
+    group_by({{ groupby }}, position) %>%
+    summarize(coverage = mean(coverage))
+  return(metadata)
+}
+
+#' @importFrom tibble as_tibble
+#' @import rlang
+#' @importFrom dplyr mutate select group_by summarize
+#' @importFrom tidyr unnest
+#' @export
+pool_gene_region_coverage.list = function(ds,
+                                          fragments_list,
+                                          gene_name,
+                                          groupby,
+                                          regions) {
+  metadata = as_tibble(ds@sampleAnnot, rownames = "sampleid")
+  metadata = metadata %>%
+    mutate(region_coverage = lapply(sampleid, function(s, r, g, ds) {
+                      frags = fragments_list[[s]]
                       cov = get_gene_region_coverage(frags, r, g)
                       return(cov)
                       }, regions, gene_name, ds))
